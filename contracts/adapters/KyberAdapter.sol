4225f9a4c8b79721ddd2771ea7328081a32bf2d2
Sean Casey <sean@seanjcasey.com>
2020-05-16 18:59:17 -0400

    refactor: update Uniswap and Kyber contracts to take either static or relative values for srcToken amount

--- contracts/adapters/KyberAdapter.sol
+++ contracts/adapters/KyberAdapter.sol
@@ -1,8 +1,14 @@
 pragma solidity ^0.6.6;
 
+import "../../node_modules/@openzeppelin/contracts/math/SafeMath.sol";
 import "../../node_modules/@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "../IStacker.sol";
 
 contract KyberAdapter {
+    using SafeMath for uint256;
+
+    uint256 constant MAX_PERCENTAGE = 10 ** 18; // 100%
+
     /// @dev Marked as payable for unit testing
     receive() external payable {}
 
@@ -18,19 +24,48 @@ contract KyberAdapter {
         (
             address srcToken,
             uint256 srcAmount,
-            // uint256 srcPercent, // TODO: should this be optionally expressable as a percent of total available balance?
+            uint256 srcPercent, // Percent of total available balance
             address destToken
-        ) = abi.decode(_callArgs, (address, uint256, address));
+        ) = abi.decode(_callArgs, (address, uint256, uint256, address));
+        require(
+            srcAmount == 0 || srcPercent == 0,
+            "takeOrder: can not specify both srcAmount and srcPercent"
+        );
+        require(
+            srcAmount > 0 || srcPercent > 0,
+            "takeOrder: must specify either srcAmount or srcPercent"
+        );
+        if (srcPercent > 0) {
+            require(srcPercent <= MAX_PERCENTAGE, "takeOrder: srcPercent must be 100% or less");
+        }
 
         // Min rate hardcoded to 1; probs fine for now
-        if (srcToken == address(0)) {
+        if (srcToken == IStacker(address(this)).ETH_ADDRESS()) {
+            if (srcAmount == 0) {
+                srcAmount = __calcRelativeAmount(payable(address(this)).balance, srcPercent);
+            }
+
             IKyberNetworkProxy(_gateway).swapEtherToToken{value: srcAmount}(destToken, 1);
         }
-        else if (destToken == address(0)) {
+        else if (destToken == IStacker(address(this)).ETH_ADDRESS()) {
+            if (srcAmount == 0) {
+                srcAmount = __calcRelativeAmount(
+                    IERC20(srcToken).balanceOf(address(this)),
+                    srcPercent
+                );
+            }
+
             IERC20(srcToken).approve(_gateway, srcAmount);
             IKyberNetworkProxy(_gateway).swapTokenToEther(srcToken, srcAmount, 1);
         }
         else {
+            if (srcAmount == 0) {
+                srcAmount = __calcRelativeAmount(
+                    IERC20(srcToken).balanceOf(address(this)),
+                    srcPercent
+                );
+            }
+
             IERC20(srcToken).approve(_gateway, srcAmount);
             IKyberNetworkProxy(_gateway).swapTokenToToken(srcToken, srcAmount, destToken, 1);
         }
@@ -39,6 +74,14 @@ contract KyberAdapter {
         receivedAssets[0] = destToken;
 
         return receivedAssets;
+    }
+
+    function __calcRelativeAmount(uint256 _amount, uint256 _percent)
+        private
+        pure
+        returns (uint256)
+    {
+        return _amount.mul(_percent).div(MAX_PERCENTAGE);
     }
 }
 
--- contracts/adapters/UniswapAdapter.sol
+++ contracts/adapters/UniswapAdapter.sol
@@ -1,12 +1,14 @@
 pragma solidity ^0.6.6;
 
+import "../../node_modules/@openzeppelin/contracts/math/SafeMath.sol";
 import "../../node_modules/@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "../IStacker.sol";
 
 contract UniswapAdapter {
-    /// @dev Marked as payable for unit testing
-    receive() external payable {}
+    using SafeMath for uint256;
 
-    /// @dev Marked as payable for unit testing
+    uint256 constant MAX_PERCENTAGE = 10 ** 18; // 100%
+
     function takeOrder(
         address _gateway,
         bytes calldata _callArgs
@@ -18,19 +20,41 @@ contract UniswapAdapter {
         (
             address srcToken,
             uint256 srcAmount,
-            // uint256 srcPercent, // TODO: should this be optionally expressable as a percent of total available balance?
+            uint256 srcPercent, // Percent of total available balance
             address destToken
-        ) = abi.decode(_callArgs, (address, uint256, address));
+        ) = abi.decode(_callArgs, (address, uint256, uint256, address));
+        require(
+            srcAmount == 0 || srcPercent == 0,
+            "takeOrder: can not specify both srcAmount and srcPercent"
+        );
+        require(
+            srcAmount > 0 || srcPercent > 0,
+            "takeOrder: must specify either srcAmount or srcPercent"
+        );
+        if (srcPercent > 0) {
+            require(srcPercent <= MAX_PERCENTAGE, "takeOrder: srcPercent must be 100% or less");
+        }
 
         // Min values all hardcoded to 1; probs fine for now
-        if (srcToken == address(0)) {
+        if (srcToken == IStacker(address(this)).ETH_ADDRESS()) {
+            if (srcAmount == 0) {
+                srcAmount = __calcRelativeAmount(payable(address(this)).balance, srcPercent);
+            }
+
             address tokenExchange = IUniswapFactory(_gateway).getExchange(destToken);
             IUniswapExchange(tokenExchange).ethToTokenSwapInput{value: srcAmount}(
                 1,
                 block.timestamp + 1
             );
         }
-        else if (destToken == address(0)) {
+        else if (destToken == IStacker(address(this)).ETH_ADDRESS()) {
+            if (srcAmount == 0) {
+                srcAmount = __calcRelativeAmount(
+                    IERC20(srcToken).balanceOf(address(this)),
+                    srcPercent
+                );
+            }
+
             address tokenExchange = IUniswapFactory(_gateway).getExchange(srcToken);
             IERC20(srcToken).approve(tokenExchange, srcAmount);
             IUniswapExchange(tokenExchange).tokenToEthSwapInput(
@@ -40,6 +64,13 @@ contract UniswapAdapter {
             );
         }
         else {
+            if (srcAmount == 0) {
+                srcAmount = __calcRelativeAmount(
+                    IERC20(srcToken).balanceOf(address(this)),
+                    srcPercent
+                );
+            }
+
             address tokenExchange = IUniswapFactory(_gateway).getExchange(srcToken);
             IERC20(srcToken).approve(tokenExchange, srcAmount);
             IUniswapExchange(tokenExchange).tokenToTokenSwapInput(
@@ -55,6 +86,14 @@ contract UniswapAdapter {
         receivedAssets[0] = destToken;
 
         return receivedAssets;
+    }
+
+    function __calcRelativeAmount(uint256 _amount, uint256 _percent)
+        private
+        pure
+        returns (uint256)
+    {
+        return _amount.mul(_percent).div(MAX_PERCENTAGE);
     }
 }
 
