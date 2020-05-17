8d7ef8c9c0cfd661b5a961e68fff5b2d31a7a7fe
Val <valmack@protonmail.com>
2020-05-16 20:16:18 -0400

    feat: add aave adapter methods

---
+++ frontend/src/lib/adapters/aave.ts
@@ -0,0 +1,19 @@
+import { AdapterMethod, getAbiFunction } from "./adapter";
+import AaveAdapterJson from "../../build/contracts/AaveAdapter.json";
+
+// TODO: takeFlashLoan requires all other adapter method parameters
+// to be nested inside its own parameters. We need a way to represent
+// and handle that.
+export const takeFlashLoan = <AdapterMethod>{
+    label: "takeFlashLoan",
+    raw: getAbiFunction(AaveAdapterJson.abi, "takeFlashLoan"),
+    parameters: ["tokenYouWant", "amountYouWant"],
+    description: "Borrow the requested token and repay the full amount in a single transaction",
+};
+
+export const payBackFlashLoan = <AdapterMethod>{
+  label: "payBackFlashLoan",
+  raw: getAbiFunction(AaveAdapterJson.abi, "payBackFlashLoan"),
+  parameters: ["gatewayAddress", "tokenYouBorrowed", "amountDue"],
+  description: "Repay the borrowed flash loan amount",
+};
