6b1a751b8c214b9ef4424fcf8e392800d60bcdee
Val <valmack@protonmail.com>
2020-05-17 21:09:21 -0400

    test: indegree0 and refactor adjacency tests

--- frontend/src/test/graph.test.ts
+++ frontend/src/test/graph.test.ts
@@ -1,7 +1,7 @@
 import {describe, expect, it} from "@jest/globals";
 
 import { Adapter } from "../lib/adapters/adapter";
-import { addNode, createGraph, createNode, addAdjacency } from "../lib/graph";
+import { addNode, createGraph, createNode, getIndegree0, addAdjacency } from "../lib/graph";
 
 describe("createGraph", function() {
   it("should return a new empty graph", function() {
@@ -32,19 +32,41 @@ describe("addNode", function() {
 });
 
 describe("addAdjacency", function() {
+  const graph_1 = createGraph<number, string>();
+  const key_1 = Date.now();
+  const node_1 = createNode<string>("A");
+  const graph_2 = addNode(graph_1, key_1, node_1);
+  const key_2 = Date.now();
+  const node_2 = createNode<string>("B");
+  const graph_3 = addNode(graph_2, key_2, node_2);
+  const condition = (value: number) => value === 1;
+  const graph_4 = addAdjacency(graph_3, key_1, key_2, condition);
+  const graph_5 = addAdjacency(graph_4, key_2, key_1);
   it("should return a copy of the given graph with the adjacency list describing the new edge", function() {
-    const graph_1 = createGraph<number, string>();
+    expect(graph_4.incomingAdjacency.get(key_2).get(key_1)).toEqual(condition);
+  });
+  it("should default the edge condition to undefined", function() {
+    expect(graph_5.incomingAdjacency.get(key_1).get(key_2)).toEqual(undefined);
+  });
+});
+
+describe("getIndegree0", function() {
+  it("should return all node keys in the graph with indegree 0", function() {
+    let graph = createGraph<number, string>();
     const key_1 = Date.now();
     const node_1 = createNode<string>("A");
-    const graph_2 = addNode(graph_1, key_1, node_1);
+    graph = addNode(graph, key_1, node_1);
+    let indegree0 = getIndegree0(graph);
+    expect(indegree0.length).toBe(1);
+    expect(indegree0).toEqual([key_1]);
+
     const key_2 = Date.now();
     const node_2 = createNode<string>("B");
-    const graph_3 = addNode(graph_2, key_2, node_2);
-    const graph_4 = addAdjacency(graph_3, key_1, key_2);
-    const condition = (value: number) => value === 1;
-    const graph_5 = addAdjacency(graph_4, key_2, key_1, condition);
-    expect(graph_4.incomingAdjacency.get(key_2).get(key_1)).toEqual(undefined);
-    expect(graph_5.incomingAdjacency.get(key_1).get(key_2)).toEqual(condition);
+    graph = addNode(graph, key_2, node_2);
+    indegree0 = getIndegree0(graph);
+    expect(indegree0.length).toBe(2);
+    expect(indegree0).toContain(key_1);
+    expect(indegree0).toContain(key_2);
   })
 });
 
