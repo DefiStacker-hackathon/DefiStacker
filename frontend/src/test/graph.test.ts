5a1984803ad80608164488a9fb43f588c8d7330c
Val <valmack@protonmail.com>
2020-05-19 09:03:45 -0400

    test: remove node and remove adjacency

--- frontend/src/test/graph.test.ts
+++ frontend/src/test/graph.test.ts
@@ -8,7 +8,8 @@ import {
   getIndegree0,
   getTopologicalSortOrder,
   addAdjacency,
-  removeAdjacency
+  removeAdjacency,
+  removeNode
 } from "../lib/graph";
 
 describe("createGraph", function() {
@@ -37,6 +38,35 @@ describe("addNode", function() {
     expect(graph_2.nodes.size).toBeGreaterThan(graph_1.nodes.size);
     expect(graph_2.nodes.get(key)).toEqual(node);
   })
+});
+
+describe("removeNode", function() {
+  it("should return a copy of the given graph with the given node removed", function() {
+    const graph_1 = createGraph<number, string>();
+    const key = Date.now();
+    const node = createNode<string>("A");
+    const graph_2 = addNode(graph_1, key, node);
+    const graph_3 = removeNode(graph_2, key);
+    expect(graph_2.nodes.get(key)).toEqual(node);
+    expect(graph_3.nodes.get(key)).toBeUndefined();
+  });
+  it("should remove all adjacency relationships that include the given node", function() {
+    const graph_1 = createGraph<number, string>();
+    const key_1 = Date.now();
+    const node_1 = createNode<string>("A");
+    const graph_2 = addNode(graph_1, key_1, node_1);
+    const key_2 = Date.now() + 1;
+    const node_2 = createNode<string>("B");
+    const graph_3 = addNode(graph_2, key_2, node_2);
+    const key_3 = Date.now() + 2;
+    const node_3 = createNode<string>("C");
+    const graph_4 = addNode(graph_3, key_3, node_3);
+    const graph_5 = addAdjacency(graph_4, key_1, key_2);
+    const graph_6 = addAdjacency(graph_5, key_2, key_3);
+    const graph_7 = removeNode(graph_6, key_1);
+    expect(graph_7.incomingAdjacency.get(key_2)).toEqual([]);
+    expect(graph_7.incomingAdjacency.get(key_3)).toEqual([key_2]);
+  });
 });
 
 describe("addAdjacency", function() {
@@ -69,6 +99,9 @@ describe("removeAdjacency", function() {
   it("should return a copy of the given graph without the described adjacency edge", function() {
     expect(graph_6.incomingAdjacency.get(key_1)).toContain(key_2);
     expect(graph_6.incomingAdjacency.get(key_2)).toEqual([]);
+  });
+  it("should not remove any nodes", function() {
+    expect(graph_5.nodes).toEqual(graph_6.nodes);
   });
 });
 
