8ed441602692ff52374653cad58fd301d861802e
Val <valmack@protonmail.com>
2020-05-19 22:25:52 -0400

    refactor: change nodeKey to id in pipeline. add doc comments

--- frontend/src/lib/pipeline.ts
+++ frontend/src/lib/pipeline.ts
@@ -1,6 +1,9 @@
-import { Graph, Node, createGraph, createNode, addNode, updateNode, removeNode, addAdjacency, clearAdjacency } from "./graph";
+import { Graph, Node, createGraph, createNode, addNode, updateNode, removeNode, addAdjacency, clearAdjacency, getTopologicalSortOrder } from "./graph";
 import { Adapter, AdapterKind, AdapterMethod, cloneAdapter } from "./adapters/adapter";
 
+/**
+ * Creates a new pipeline with no adapters.
+ */
 export function startNewPipeline(): Graph<Node<Adapter>, number>{
   return createGraph();
 }
@@ -9,8 +12,8 @@ export function startNewPipeline(): Graph<Node<Adapter>, number>{
  * Returns a new pipeline with the adapter added and the added adapter's id.
  * @param pipeline 
  * @param adapter 
- * @param incoming Keys for adapters that should point to the added adapter
- * @param outgoing Keys for adapters that the added adapter should point to
+ * @param incoming Ids for adapters that should point to the added adapter
+ * @param outgoing Ids for adapters that the added adapter should point to
  */
 export function addAdapter(
   pipeline: Graph<Node<Adapter>, number>,
@@ -34,41 +37,49 @@ export function addAdapter(
   return { pipeline: pipeline, adapterId: nodeKey };
 }
 
+/**
+ * Returns a clone of `pipeline` with the adapter of id `id` modified.
+ * @param pipeline 
+ * @param id Id of the the adapter to update.
+ * @param kind
+ * @param method
+ * @param args
+ */
 export function updateAdapter(
   pipeline: Graph<Node<Adapter>, number>,
-  nodeKey: number,
+  id: number,
   kind?: AdapterKind,
   method?: AdapterMethod,
   args?: Array<string>
 ): any {
-  let adapter = pipeline.nodes.get(nodeKey).value;
+  let adapter = pipeline.nodes.get(id).value;
   adapter = cloneAdapter(adapter, kind, method, args);
-  pipeline = updateNode(pipeline, nodeKey, adapter);
-  return { pipeline: pipeline, adapterId: nodeKey };
+  pipeline = updateNode(pipeline, id, adapter);
+  return { pipeline: pipeline, adapterId: id };
 }
 
 export function removeAdapter(
   pipeline: Graph<Node<Adapter>, number>,
-  nodeKey: number
+  id: number
 ): any {
 
-  const incoming: Array<number> = pipeline.incomingAdjacency.get(nodeKey);
+  const incoming: Array<number> = pipeline.incomingAdjacency.get(id);
   const outgoing: Array<number> = [];
   pipeline.incomingAdjacency.forEach((list, key) => {
-    if (list.indexOf(nodeKey) > -1) {
+    if (list.indexOf(id) > -1) {
       outgoing.push(key);
     }
   });
 
   // Remap incoming to outgoing of the removed adapter
-  pipeline = removeNode(pipeline, nodeKey);
+  pipeline = removeNode(pipeline, id);
   incoming.forEach((inKey) => {
     outgoing.forEach((outKey) => {
       pipeline = addAdjacency(pipeline, inKey, outKey);
     });
   });
 
-  return { pipeline: pipeline, adapterId: nodeKey };
+  return { pipeline: pipeline, adapterId: id };
 }
 
 export function serializePipeline(pipeline: Graph<Node<Adapter>, number>): string {
@@ -82,6 +93,10 @@ export function serializePipeline(pipeline: Graph<Node<Adapter>, number>): strin
   });
 }
 
+/**
+ * Returns a pipeline object based on the `serialized` version.
+ * @param serialized The stringified pipeline.
+ */
 export function deserializePipeline(serialized: string): Graph<Node<Adapter>, number>{
   let pipeline = JSON.parse(serialized);
   return {
