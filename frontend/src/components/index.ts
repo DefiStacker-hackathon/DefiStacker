42704e770fd74259758bed75767bee49cb36bd0d
Justin Kaseman <justinkaseman@live.com>
2020-05-20 16:42:06 -0700

    Refactor: cleanup app & index files. Remove placeholder ANTVersion component

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -1,10 +1,10 @@
-import { ANTVersion } from './ANTVersion';
 import { Form } from './Form';
 import { Chain } from './Chain';
 import { Block } from './Block';
 import { InitialBlock } from './InitialBlock';
+import { Landing } from './Landing';
 
-export { ANTVersion };
+export { Landing };
 export { Form };
 export { Chain };
 export { Block };
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,29 +1,18 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import * as ethers from 'ethers';
 import { App } from './App';
-import Context from './context';
+import { PipelineProvider } from './context';
 import { createClient, Provider } from 'urql';
 
-const client = createClient({
+const subgraph = createClient({
   url: 'http://localhost:8000/subgraphs/name/defistacker/defistacker',
 });
 
-const url = 'http://localhost:8545';
-
-// Or if you are running the UI version, use this instead:
-// const url = "http://localhost:7545"
-
-const provider = new ethers.providers.JsonRpcProvider(url);
-const ctx = { provider };
-
-export const EthersContext = React.createContext(ctx);
-
 ReactDOM.render(
-  <Provider value={client}>
-    <Context.Provider value={ctx}>
+  <Provider value={subgraph}>
+    <PipelineProvider>
       <App />
-    </Context.Provider>
+    </PipelineProvider>
   </Provider>,
   document.getElementById('app'),
 );
