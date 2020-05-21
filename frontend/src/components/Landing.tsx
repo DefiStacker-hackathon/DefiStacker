0d65d368b7fc19f5e11f07e2fdee4946f96a7652
Justin Kaseman <justinkaseman@live.com>
2020-05-21 14:32:07 -0700

    feat: clean up responsiveness of Landing Page

--- frontend/src/components/Landing.tsx
+++ frontend/src/components/Landing.tsx
@@ -2,6 +2,11 @@ import * as React from 'react';
 import { Row, Col, Button } from 'antd';
 import { usePipelineDispatch } from '../context';
 
+const ColStyle: React.CSSProperties = {
+  marginTop: '10px',
+  marginBottom: '10px',
+};
+
 const Landing: React.FC = () => {
   const dispatch = usePipelineDispatch();
 
@@ -13,33 +18,33 @@ const Landing: React.FC = () => {
         paddingTop: '80px',
       }}
     >
-      <Col span={12} style={{ minWidth: '400px' }} flex={1}>
+      <Col span={24} md={12}>
         <Row justify="center">Welcome to DeFi Stacker! We do some stuff.</Row>
       </Col>
-      <Col span={11} flex={1}>
+      <Col span={24} md={12}>
         <Row justify="center" style={{ paddingBottom: '30px' }}>
-          What would you like to do today?
+          What would you like to do with DeFi today?
         </Row>
         <Row justify="center">
-          <Col>
+          <Col style={ColStyle}>
             <Row justify="center">
               <Button size="large" type="primary">
                 <Row>Liquidate a CDP</Row>
               </Button>
             </Row>
           </Col>
-          <Col offset={1}>
+          <Col offset={1} style={ColStyle}>
             <Button size="large" type="primary">
               <Row>Recollateralize a loan</Row>
             </Button>
           </Col>
-          <Col offset={1}>
+          <Col offset={1} style={ColStyle}>
             <Button
               size="large"
-              type="primary"
               onClick={() => dispatch({ type: 'init' })}
+              type="dashed"
             >
-              <Row>Custom</Row>
+              <Row>Build my own transaction</Row>
             </Button>
           </Col>
         </Row>
