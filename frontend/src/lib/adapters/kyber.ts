a2a8b372a8dac0918c1d985a442e905894c6e645
Justin Kaseman <justinkaseman@live.com>
2020-05-22 16:30:39 -0700

    feat: Remove ABI reference from Adapter methods

--- frontend/src/lib/adapters/kyber.ts
+++ frontend/src/lib/adapters/kyber.ts
@@ -1,9 +1,10 @@
-import { AdapterMethod, getAbiFunction } from "./adapter";
-import KyberAdapterJson from "../../build/contracts/KyberAdapter.json";
+import { AdapterMethod } from './adapter';
 
 export const swap = <AdapterMethod>{
-    label: "swap",
-    raw: getAbiFunction(KyberAdapterJson.abi, "takeOrder"),
-    parameters: ["gatewayAddress", ["tokenYouHave", "amountToSwap", "tokenYouWant"]],
-    description: "Swaps the token you have for the token you want",
+  label: 'swap',
+  parameters: [
+    'gatewayAddress',
+    ['tokenYouHave', 'amountToSwap', 'tokenYouWant'],
+  ],
+  description: 'Swaps the token you have for the token you want',
 };
